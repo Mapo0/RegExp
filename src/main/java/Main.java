540d2883825eebe9ba86a746d9e23e012ced11b0
Map0 <gabdra-marat@yandex.ru>
2018-12-04 22:18:53 +0400

    asd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,13 @@
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("\\+\\d+\\s(\\d{3})\\s\\d{3}\\s\\d{2}\\s\\d{2}");
-        Matcher m = p.matcher("Notification has been sent 150 times to +4 351 455 22 44 successfully. +1(431) 542 56 12 is unreachable");
-        if (m.find()) {
-            System.out.print("вывод" + m.start() + m.group());
-        } else {
-            System.out.print("lol");
-        }
+        SearchPhoneNum searchPhoneNum = new SearchPhoneNum();
+        System.out.println(searchPhoneNum.read());
+        System.out.println(searchPhoneNum.selectNum());
+        searchPhoneNum.write();
     }
+
 }
--- src/main/java/SearchPhoneNum.java
+++ src/main/java/SearchPhoneNum.java
@@ -1,18 +1,56 @@
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SearchPhoneNum {
-    private static final String Phones = "C:\\Users\\Marat_Gabdrakhmanov\\IdeaProjects\\RegExp\\phones.txt";
-    private static final String PhonesFormatted = "C:\\Users\\Marat_Gabdrakhmanov\\IdeaProjects\\RegExp\\phones.txt";
+    private static final String Phones = "C:\\Users\\User\\IdeaProjects\\RegExp\\phones.txt";
+    private static final String PhonesFormatted = "C:\\Users\\User\\IdeaProjects\\RegExp\\newPhones.txt";
 
 
-    private void readerNum() throws FileNotFoundException {
-        Reader reader = new FileReader("phones.txt");
+    public   String read()  {
+        String result="";
+        try {
+            Reader reader = new FileReader("phones.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> Read = null;
+        try {
+            Read = Files.readAllLines(Paths.get(Phones),StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(String line: Read){
+            result+=line;
+        }
+        return result;
+    }
 
+    public String selectNum()  {
+        String result="";
+        Pattern p = Pattern.compile("\\+\\d+\\s(\\d{3})\\s\\d{3}\\s\\d{2}\\s\\d{2}");
+        Matcher m =p.matcher(read());
+        while (m.find()){
+            result=result.concat(m.group().replaceAll("\\D", "")+"\n");
+        }
+        return result;
+    }
+    public void write(){
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(PhonesFormatted), "utf-8"))) {
+            writer.write(selectNum());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
