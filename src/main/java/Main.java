753c432f38102776c2858d538720b347b5ef8a48
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2018-12-04 16:34:50 +0400

    asd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("\\+\\d+\\s\\d{3}\\s\\d{3}\\s\\d{2}\\s\\d{2}");
+        Pattern p = Pattern.compile("\\+\\d+\\s(\\d{3})\\s\\d{3}\\s\\d{2}\\s\\d{2}");
         Matcher m = p.matcher("Notification has been sent 150 times to +4 351 455 22 44 successfully. +1(431) 542 56 12 is unreachable");
         if (m.find()) {
             System.out.print("вывод" + m.start() + m.group());
--- src/main/java/SearchPhoneNum.java
+++ src/main/java/SearchPhoneNum.java
@@ -1,4 +1,7 @@
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,18 +10,9 @@ public class SearchPhoneNum {
     private static final String PhonesFormatted = "C:\\Users\\Marat_Gabdrakhmanov\\IdeaProjects\\RegExp\\phones.txt";
 
 
-    private void phoneNum() {
-        String inputString = "Sample line of text: \"Notification has been sent 150 times to +4(351) 455 22 44 successfully. +1(431) 542 56 12 is unreachable";
-        String pattern = "\\+\\d+\\s(\\d{3})\\s\\d{3}\\s\\d{2}\\s\\d{2}";
+    private void readerNum() throws FileNotFoundException {
+        Reader reader = new FileReader("phones.txt");
 
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(inputString);
-
-        if (m.find()) {
-            System.out.print("Phone number" + m.group());
-        } else {
-            System.out.print("not found");
-        }
 
     }
 }
